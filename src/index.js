68e151a385172320e24ae99a21839bfd19f32823
Jensen Bernard <jensenbernard5@gmail.com>
2017-03-13 23:40:01 +0100

    Add tests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 // @flow
 
-export default {
-  add: (a: number, b: number) => a + b,
-  subtract: (a: number, b: number) => a - b,
-};
+import fs from 'fs';
+import path from 'path';
+
+// $FlowFixMe: Can't find ad-block... whatever.
+import { AdBlockClient, FilterOptions } from 'ad-block'; // eslint-disable-line
+
+const client = new AdBlockClient();
+
+export const initialize = () =>
+  new Promise((resolve, fail) => {
+    const bufferPath = path.resolve(__dirname, './detector.buffer');
+    fs.readFile(bufferPath, (err, buffer) => {
+      if (err) { fail(err); }
+      client.deserialize(buffer);
+      resolve();
+    });
+  });
+
+export const isAd = (request: string, base: string = '') =>
+  client.matches(request, FilterOptions.noFilterOption, base);
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,17 @@
 import { expect } from 'chai';
-import index from '../src/index';
+import { initialize, isAd } from '../src/index';
 
 describe('Default tests', () => {
-  it('should add', () => { expect(index.add(2, 4)).to.equal(6); });
-  it('Should subtract', () => { expect(index.subtract(2, 4)).to.equal(-2); });
+  it('should detect ad', (done) => {
+    initialize().then(() => {
+      expect(isAd('http://www.somesite.com/public/ad/some-ad')).to.equal(true);
+      done();
+    }).catch(done);
+  });
+  it('should not detect twitter', (done) => {
+    initialize().then(() => {
+      expect(isAd('http://www.twitter.com')).to.equal(false);
+      done();
+    }).catch(done);
+  });
 });
